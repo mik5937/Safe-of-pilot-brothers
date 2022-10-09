c36bc8c050e6206d941c2141322d4fc115f5087a
mik5937 <54955142+mik5937@users.noreply.github.com>
2022-10-09 17:04:06 +0300

    Add files via upload

---
+++ Safe of pilot brothers/Game.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Safe_of_pilot_brothers
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+      
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(931, 92);
+            this.label1.MaximumSize = new System.Drawing.Size(200, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Нажмите кнопку выше, чтобы сгенерировать себе условие. Если будете уверены, что с" +
+    "делали правильно, нажмите кнопку ниже чтобы проверить.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(934, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать задачу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(934, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(956, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 814);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Game";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Safe of pilot brothers/Game.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Safe_of_pilot_brothers
+{
+
+    public partial class Game : Form
+    {
+        private  int n = 2 ;
+       
+        int[,] matrix = new int[2, 2];
+        Button[,] spots = new Button[2, 2];
+        public Game(int N)
+        {
+           InitializeComponent();
+            n = N;
+        }
+        void ResizeArray<T>(ref T[,] original, int newCoNum, int newRoNum)
+        {
+            var newArray = new T[newCoNum, newRoNum];
+            int columnCount = original.GetLength(1);
+            int columnCount2 = newRoNum;
+            int columns = original.GetUpperBound(0);
+            for (int co = 0; co <= columns; co++)
+                Array.Copy(original, co * columnCount, newArray, co * columnCount2, columnCount);
+            original = newArray;
+        }
+        int  MainMenu()
+        {
+ 
+                int index = 0;
+                for (int x = 0; x < spots.GetLength(0); ++x)
+                    for (int y = 0; y < spots.GetLength(1); ++y)
+                    {
+                        index++;
+                        spots[x, y] = new Button();
+                        spots[x, y].Size = new Size(40, 40);
+                        spots[x, y].Location = new Point(x * 40, y * 40);
+                        spots[x, y].Tag = index;
+                        Controls.Add(spots[x, y]); spots[x, y].Click += new EventHandler(Form1_Click);
+                        this.Controls.Add(spots[x, y]);
+                        spots[x, y].BringToFront();            
+                    }
+            return 0;
+        }
+        void UpdateColor()
+        {
+            for (int x = 0; x < n; ++x)
+            {
+                for (int y = 0; y < n; ++y)
+                {
+                    if (matrix[x, y] == 1)
+                    {
+                        spots[x, y].BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        if (matrix[x, y] == 0) { spots[x, y].BackColor = Color.Red; }
+                    }
+                }
+            }
+        }
+      
+        private void Game_Load(object sender, EventArgs e)
+        {
+            ResizeArray<int>(ref matrix, n, n);
+            ResizeArray<Button>(ref spots, n, n);
+            MainMenu();
+
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            ChangeMatrix(Convert.ToInt32(button.Tag));   
+        }
+
+        int ChangeMatrix(int Tag)
+        {
+            int x,y = 0;
+            x = (Tag-1) / n;
+            y = (Tag-1) % n;
+            for (int i = 0; i < n; i++)
+            {
+                if (matrix[x, i] == 0)
+                {matrix[x, i] = 1;}
+                else
+                { matrix[x, i] = 0;}
+                if (matrix[i, y] == 0)
+                { matrix[i, y] = 1;}
+                else
+                {matrix[i, y] = 0;}
+            }
+            if (matrix[x, y] == 0)
+            {matrix[x, y] = 1;}
+            else { matrix[x, y] = 0; }
+            UpdateColor();
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Random k = new Random();
+            int Step = r.Next(7, 15);
+            for(int i = 0; i < Step; i++)
+            {              
+                int Tag = k.Next(1, n*n-1);
+                ChangeMatrix(Tag);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+       
+            int sum=0;
+            for (int i =0; i<n;i++)
+            {
+                for (int j = 0; j < n; j++)
+                { sum = sum + matrix[i, j]; }
+            }          
+            if (sum==0 | sum == n*n)
+            { label1.Text = "Вы выиграли!"; }
+            else { label1.Text = "Пока не решено."; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
+
